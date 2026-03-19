54f75a1a8ab7ad4ca2a19ed36519572a239808d6
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2026-03-18 21:30:45 -0700

    fat: enable hardware graphics and steam on thinkpad

--- hosts/thinkpad/default.nix
+++ hosts/thinkpad/default.nix
@@ -7,14 +7,28 @@
     ../../common/desktop
   ];
 
+  users.users.ppanda = {
+    extraGroups = [ "video" "audio" ];
+    packages = with pkgs; [
+      docker
+      docker-compose
+      claude-code
+      github-copilot-cli
+      opencode
+      go
+      mangohud
+      protonup-qt
+      python315
+      uv
+    ];
+  };
+
   networking.hostName = "thinkpad";
   time.timeZone = "America/Los_Angeles";
 
   # ThinkPad P14s Gen 5 Intel
   boot.kernelModules = [ "thinkpad_acpi" ];
   services.thermald.enable = true;
-
-  environment.systemPackages = [ pkgs.brightnessctl ];
 
   # Trackpad
   services.libinput = {
@@ -27,21 +41,23 @@
   # Fingerprint reader
   services.fprintd.enable = true;
 
+  # Graphics (Intel Iris Xe)
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  # Steam
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+  };
+  programs.gamemode.enable = true;
+
   # Bluetooth
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
-
-  users.users.ppanda = {
-    extraGroups = [ "video" "audio" ];
-    packages = with pkgs; [
-      docker
-      docker-compose
-      github-copilot-cli
-      go
-      python315
-      uv
-    ];
-  };
 
   system.stateVersion = "25.11";
 }
