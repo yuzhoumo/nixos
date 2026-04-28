ac5bf39e12bdfd4d1613b874f98c4c9bf9e9643c
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2026-04-28 00:28:14 -0700

    feat: add ssh pubkey to user

--- common/all/users.nix
+++ common/all/users.nix
@@ -7,5 +7,8 @@
     extraGroups = [ "wheel" ];
     shell = pkgs.zsh;
     home = "/home/ppanda";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPt6a7BXOx2ceDfve7vZai7y4Avo7ylUm/x1JRYrjErY ppanda@nixos"
+    ];
   };
 }
