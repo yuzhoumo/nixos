b519e0b646a8c8c3406abe009214d068a7ee050d
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2026-04-30 09:01:52 -0700

    feat: add power managment/misc tweaks for optiplex

--- hosts/optiplex/default.nix
+++ hosts/optiplex/default.nix
@@ -10,7 +10,17 @@
     ../../services/tailscale
   ];
 
-  hardware.cpu.intel.updateMicrocode = true;
+  hardware.cpu.intel.updateMicrocode = true; # meltdown/spectre tweaks
+
+  powerManagement {
+    cpuFreqGovernor = "schedutil"; # cpu frequency scaling
+    powertop.enable = true; # auto-tune USB/PCIe/SATA power management
+  }
+
+  services {
+    fstrim.enable = true; # enable ssd trim
+    thermald.enable = true; # intel thermal management daemon
+  };
 
   networking.hostName = "optiplex";
   time.timeZone = "America/Los_Angeles";
