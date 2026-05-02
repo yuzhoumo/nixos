cfd4d7e5df30876104ae717bffb8fcdf2d961725
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2026-05-02 13:45:50 -0700

    feat: add ssh pubkey

--- roles/all/users.nix
+++ roles/all/users.nix
@@ -9,6 +9,7 @@
     home = "/home/ppanda";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPt6a7BXOx2ceDfve7vZai7y4Avo7ylUm/x1JRYrjErY ppanda@nixos"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINlL9baqTTX+nqQroKpRpmIjRuW0beX0fCB4uIYxVLcx ppanda@esper"
     ];
   };
 }
