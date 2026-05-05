afa4a7a628c69b7638d432bf0d6dc9fc4f86f5a7
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2026-05-05 00:12:13 -0700

    fix: docker networking

--- services/tailscale/default.nix
+++ services/tailscale/default.nix
@@ -13,11 +13,4 @@
     allowedUDPPorts = [ config.services.tailscale.port ]; # allow Tailscale UDP port through the firewall
     checkReversePath = "loose"; # fix firewall issues when enabling exit node (https://github.com/tailscale/tailscale/issues/4432#issuecomment-1112819111)
   };
-
-  # force tailscaled to use nftables (critical for clean nftables-only systems)
-  # avoids "iptables-compat" translation layer issues
-  networking.nftables.enable = true;
-  systemd.services.tailscaled.serviceConfig.Environment = [
-    "TS_DEBUG_FIREWALL_MODE=nftables"
-  ];
 }
