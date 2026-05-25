d842007d2af6c4486001b294b652f0114b624d62
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2026-05-25 11:32:14 -0700

    feat: add teams-for-linux intune auth config

--- hosts/thinkpad/work.nix
+++ hosts/thinkpad/work.nix
@@ -36,4 +36,18 @@
     entraUser = "joemo@microsoft.com";
     wsl = false;
   };
+
+  system.activationScripts.teams-for-linux-config.text = let
+    configFile = pkgs.writeText "teams-for-linux-config.json" (builtins.toJSON {
+      auth.intune = {
+        enabled = true;
+        user = "joemo@microsoft.com";
+      };
+    });
+    home = config.users.users.joemo.home;
+  in ''
+    mkdir -p "${home}/.config/teams-for-linux"
+    cp -f "${configFile}" "${home}/.config/teams-for-linux/config.json"
+    chown -R joemo:$(id -gn joemo) "${home}/.config/teams-for-linux"
+  '';
 }
