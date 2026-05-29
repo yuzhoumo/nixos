14dd90e51aab58508a93801bd6f8199d873f7621
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2026-05-29 11:57:29 -0700

    feat: add firefox module to wsl-work host

--- hosts/wsl-work/default.nix
+++ hosts/wsl-work/default.nix
@@ -5,6 +5,7 @@
     ../../modules/common
     ../../modules/developer
     ../../modules/users/joemo.nix
+    ../../modules/firefox
   ];
 
   programs.azurevpn.enable = true;
@@ -28,10 +29,8 @@
     usbutils # lsusb
   ];
 
-  # Firefox for browser SSO via linux-entra-sso (auto-configured by himmelblau module)
   # Force d3d12 gallium driver instead of zink to avoid Mesa/EGL GPU crashes on WSL
   # Force XWayland to work around broken context menus under native Wayland on WSL
-  programs.firefox.enable = true;
   environment.variables.GALLIUM_DRIVER = "d3d12";
   environment.variables.MOZ_ENABLE_WAYLAND = "0";
   environment.variables.MOZ_GTK_TITLEBAR_DECORATION = "client";
