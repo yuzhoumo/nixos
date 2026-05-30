cd3c254b23178ba40e30c098baa62a77c66a0359
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2026-05-30 12:01:46 -0700

    fix: add shared libs for foreign binaries to run

--- modules/developer/default.nix
+++ modules/developer/default.nix
@@ -12,4 +12,7 @@
 
   # generate apropos, whatis, man -k cache
   documentation.man.cache.enable = true;
+
+  # provides /lib64/ld-linux-x86-64.so.2, allowing foreign binaries to run
+  programs.nix-ld.enable = true;
 }
